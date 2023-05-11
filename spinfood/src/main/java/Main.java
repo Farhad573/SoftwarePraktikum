0892018f91a4c49afe37219177b6fb497bd1fec4
danielSajjadi <sajjadi@student.uni-marburg.de>
2023-05-11 14:05:52 +0200

    clean main class

--- spinfood/src/main/java/Main.java
+++ spinfood/src/main/java/Main.java
@@ -6,28 +6,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) {
-        //new View();
         ParticipantModel participantModel = new ParticipantModel();
-
-/*
-        try {
-            participantModel.readCSVData_1("src/main/java/model/teilnehmerliste.csv");
-            System.out.println(participantModel.toString_1());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("**************************************************");
-*/
-
-//
-//        try {
-//            participantModel.readCSVData_2("src/main/java/model/teilnehmerliste.csv");
-//            System.out.println(participantModel.toStringParticipants());
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-
 
         try {
             participantModel.readCSVFile("src/main/java/model/teilnehmerliste.csv");
